598b4bb15969eae3674cb86c31c6e15702d762c2
profedsonoliveira <prof.edson.oliveira@hotmail.com>
2024-07-19 20:17:59 -0300

    Criação do projeto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+internal class Program
+{
+    static int prox(int n)
+    {
+        return n + 1;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, sss World!");
+        Console.WriteLine(prox(6));
+    }
+}
+
+
